fa227a3ca9bb05128af9d9676f836e83039ac470
loadercc <60961630+Skills-png@users.noreply.github.com>
2021-07-20 16:15:09 +0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,17 @@
+import sys, os
+from socket import socket, AF_INET, SOCK_DGRAM
+SERVER_IP   = sys.argv[1] #    172.16.51.14
+PORT_NUMBER = sys.argv[2] #    5000
+SIZE = 1024
+print('[*] Metros Jconsole - Remote computer access.')
+print('[*] Jconsole version 1.0.\n')
+print('[*] Github client: https://github.com/Skills-png/jconsoleclient')
+print('[*] Github server: https://github.com/Skills-png/jconsoleserver\n')
+try:
+    mySocket = socket(AF_INET, SOCK_DGRAM)
+    print('[+] Ip '+SERVER_IP+' Port '+PORT_NUMBER)
+except:
+    print('[-] Ip '+SERVER_IP+' Port '+str(PORT_NUMBER))
+while True:
+    mySocket.sendto(input(' $ ').encode('utf8'),(SERVER_IP,PORT_NUMBER))
+sys.exit()
