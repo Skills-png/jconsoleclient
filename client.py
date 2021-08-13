c8c8e52ff7124f39d9fcdd333e1d5029763b2019
loadercc <60961630+Skills-png@users.noreply.github.com>
2021-08-13 14:19:03 +0500

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ SERVER_IP   = sys.argv[1] #    172.16.51.14
 PORT_NUMBER = sys.argv[2] #    5000
 SIZE = 1024
 print('[*] Metros Jconsole - Remote computer access.')
-print('[*] Jconsole version 1.0.\n')
+print('[*] Jconsole version 1.1.\n')
 print('[*] Github client: https://github.com/Skills-png/jconsoleclient')
 print('[*] Github server: https://github.com/Skills-png/jconsoleserver\n')
 try:
